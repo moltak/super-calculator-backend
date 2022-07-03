e55b9f6be7fd9b8aa6f9c9b85765ef86a129e547
kh <moltak@gmail.com>
2022-07-03 16:53:35 +0900

    Procfile

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
 import { NestFactory } from '@nestjs/core';
+import { env } from 'process';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3001);
+
+  console.log(`PORT: ${process.env.PORT}`);
 }
 bootstrap();
